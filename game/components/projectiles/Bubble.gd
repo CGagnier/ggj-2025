2a0cde634e77327b4a9a5a19f62c857089d16750
Jeremie Roy <j-roy95@hotmail.com>
2025-01-25 13:40:02 -0500

    Fix shooting mechanic

--- game/components/projectiles/Bubble.gd
+++ game/components/projectiles/Bubble.gd
@@ -13,32 +13,40 @@ var inflate_percent := 0.0
 
 var t := 0.0
 
+var collided = false
+var body_started_in_bubble = false
+
 func _physics_process(delta: float) -> void:
 	if launched:
 		t += delta * 2
 		t = min(t, 1.0)
 		
+		# Activate bounce after a small delay
 		speed = lerpf(start_speed, final_speed, t)
 		
 		var collision = move_and_collide(dir *  speed)
-		if collision:
+		
+		if collision and not collided:
 			if collision.get_collider() is Bubble:
 				var scene:PackedScene = load(scene_file_path)
 				var new_instance = scene.instantiate()
 				
 				var other_bubble = collision.get_collider() as Bubble
+				other_bubble.collided = true # prevent other bubble from detecting same collision
 				
 				collision.get_collider().queue_free()
 				queue_free()
 				get_parent().add_child(new_instance)
 				
 				#new_instance.global_position = (other_bubble.global_position - global_position) / 2 + global_position
-				var total_length = (global_scale.length() + other_bubble.global_scale.length()) / 2
+				var total_length = 0.5*(global_scale.length() + other_bubble.global_scale.length())
 				#ce bout la est degueulasse je vais le changer
-				new_instance.get_node("JumpPad").monitoring = true
+				
 				new_instance.global_position = global_position
 				new_instance.scale = scale
-				new_instance.speed = 0
+				new_instance.start_speed = 0
+				new_instance.final_speed = 0
+				new_instance.launched = true
 				
 				var collision_tween = get_tree().create_tween()
 				var target_position = (other_bubble.global_position - global_position) / 2 + global_position
@@ -55,22 +63,34 @@ func release() -> void:
 	# move slower
 	const _inflate_percent_influence = 0.7
 	final_speed = final_speed * (1-inflate_percent * _inflate_percent_influence)
-	$JumpPad.monitoring = true
 
 func _on_jump_pad_body_entered(body: Node2D) -> void:
+	# todo: Restrict this to Player class? 
+	if not launched:
+		body_started_in_bubble = true
+			
 	if body.velocity.y > 0:
-		#var bubble_speed_influence = (dir * speed).dot(Vector2.UP) * 100
-		
-		speed = 0
-		var tween = get_tree().create_tween()
-		body.velocity.y = 0
-		
-		var base_scale = scale
-		#tween.tween_property(self, "scale", scale * Vector2(1, 0.9), 0.05).set_trans(Tween.TRANS_BOUNCE)
-		#tween.tween_property(self, "scale", base_scale, 0.05).set_trans(Tween.TRANS_CUBIC)
-		var start_position_y = $Sprite2D.position.y
-		tween.tween_property($Sprite2D, "position:y", start_position_y + 1, 0.05).set_trans(Tween.TRANS_BOUNCE)
-		var scale_modifier = scale.length() * 0.5
-		var jump_force = body.JUMP_VELOCITY * scale_modifier
-		tween.tween_property(body, "velocity:y", jump_force, 0.01).set_trans(Tween.TRANS_BOUNCE)
-		tween.tween_callback(queue_free)
+		if launched and not body_started_in_bubble:
+			# Prevent the player from boucning around when shooting left or right while in the bubble
+			# Note: This does not work.
+			
+			speed = 0
+			var tween = get_tree().create_tween()
+			body.velocity.y = 0
+			
+			var base_scale = scale
+			#tween.tween_property(self, "scale", scale * Vector2(1, 0.9), 0.05).set_trans(Tween.TRANS_BOUNCE)
+			#tween.tween_property(self, "scale", base_scale, 0.05).set_trans(Tween.TRANS_CUBIC)
+			var start_position_y = $Sprite2D.position.y
+			tween.tween_property($Sprite2D, "position:y", start_position_y + 1, 0.05).set_trans(Tween.TRANS_BOUNCE)
+			var scale_modifier = scale.length() * 0.5
+			var jump_force = body.JUMP_VELOCITY * scale_modifier
+			tween.tween_property(body, "velocity:y", jump_force, 0.01).set_trans(Tween.TRANS_BOUNCE)
+			tween.tween_callback(queue_free)
+		else:
+			body_started_in_bubble = true
+
+
+func _on_jump_pad_body_exited(body: Node2D) -> void:
+	if not launched:
+		body_started_in_bubble = false
--- game/components/projectiles/bubble_shooter.gd
+++ game/components/projectiles/bubble_shooter.gd
@@ -7,35 +7,80 @@ extends Node2D
 
 var current_projectile: Node2D = null
 
-var shoot_dirs = ["shoot_left", "shoot_up", "shoot_right", "shoot_down"]
+const shoot_dirs = ["shoot_left", "shoot_up", "shoot_right", "shoot_down"]
+
+var last_pressed_timestamps = [0.0, 0.0, 0.0, 0.0]
 
 var input_to_dir: Dictionary[String, Vector2] = {
-	"shoot_left": Vector2.LEFT,
-	"shoot_up": Vector2.UP,
-	"shoot_right": Vector2.RIGHT,
-	"shoot_down": Vector2.DOWN
+	shoot_dirs[0]: Vector2.LEFT,
+	shoot_dirs[1]: Vector2.UP,
+	shoot_dirs[2]: Vector2.RIGHT,
+	shoot_dirs[3]: Vector2.DOWN
 }
 
 var current_shoot_dir = Vector2.RIGHT
 
+var release_time := 0.0
+var released = false
+var release_to_let_go_time := 0.1
+
 func _input(event: InputEvent) -> void:
 	pass
+	
+func _process(delta: float) -> void:
+	var offset = 10 * current_shoot_dir
+	if current_projectile:
+		current_projectile.position = offset
+	
 
 func _physics_process(delta: float) -> void:
-	for shoot_dir in shoot_dirs:
+	if released:
+		release_time += delta
+		
+		if release_time >= release_to_let_go_time:
+			_let_go()
+			released = false
+			release_time = 0
+	
+	var last_index = -1
+	var last_pressed_timestamp = -1
+	
+	for i in 4:
+		var shoot_dir = shoot_dirs[i]
 		if Input.is_action_just_pressed(shoot_dir) and not current_projectile:
+			last_pressed_timestamps[i] = Time.get_unix_time_from_system()
 			_create_bubble()
-
+			
+			# Keep tracked of which key was pressed last.
+			var ts = last_pressed_timestamps[i]
+			if ts > last_pressed_timestamp:
+				last_pressed_timestamp = ts
+				last_index = i
+	
 	for shoot_dir in shoot_dirs:
 		if Input.is_action_pressed(shoot_dir):
+			released = false
+			release_time = 0
+			current_shoot_dir = input_to_dir.get(shoot_dir)
+			
 			if current_projectile and current_projectile.scale < max_scale: 
 				current_projectile.global_scale *= (1 + growth_rate * delta)
-				current_shoot_dir = input_to_dir.get(shoot_dir)
 				break
+			
 	
 	for shoot_dir in shoot_dirs:
 		if input_to_dir.get(shoot_dir) == current_shoot_dir and Input.is_action_just_released(shoot_dir):
-			_let_go()
+			if Time.get_unix_time_from_system() - last_pressed_timestamp > 0.05:
+				# This code path is to handle quickly shooting small projectiles
+				_let_go()
+				released = false
+				release_time = 0
+				last_pressed_timestamps = [0.0, 0.0, 0.0, 0.0]
+			else:
+				# Start a timer to release this bubble shortly.
+				# It's to avoid a case where the player switches shoot direction, so there's a small gap in time
+				# where all keys are released.
+				released = true
 
 
 func _create_bubble() -> void:
