79d6a0569737dab00db50ba8ccc099e11081b310
Julien Esposito <julespo3d@gmail.com>
2025-01-25 16:33:17 -0500

    Cannon with ball base class

---
+++ game/ball_stat.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name BallStat
+
+@export var speed:float = 100.0
+@export var gravity_scale:float = 0.0
+@export var timeToDestroy:float = 0.0
+@export var lock_rotation:bool = true
