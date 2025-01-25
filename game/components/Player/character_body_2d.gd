3314171ee5c32731816482f6865250354658768e
Julien Esposito <julespo3d@gmail.com>
2025-01-25 17:41:43 -0500

    Add Jump Down + Controller inputs

--- game/components/Player/character_body_2d.gd
+++ game/components/Player/character_body_2d.gd
@@ -4,6 +4,7 @@ class_name Player
 signal died()
 
 @export var SPEED = 140.0
+@export var SPEED_JUMPDOWN = 2000.0
 const JUMP_VELOCITY = -400.0
 
 var will_die = false
@@ -73,6 +74,7 @@ func _process_falling():
 func _process_jumping():
 	if velocity.y >= 0:
 		_enter_state(state.falling)
+
 #endregion
 
 func _ready() -> void:
@@ -84,20 +86,21 @@ func _physics_process(delta: float) -> void:
 
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		var InputDown = Vector2(0,Input.get_action_strength("move_down")*SPEED_JUMPDOWN*delta)
+		velocity += get_gravity() * delta + InputDown
+		
 		# TODO: Remove dying trigger
-		if velocity.y > 600:
+		if velocity.y > 2000:
 			will_die = true
 		# Not falling in the context of a jump
 		_try_change_state(state.falling)
 	
 	if alive:
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			_enter_state(state.jump)
-
+		
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction := Input.get_axis("move_left", "move_right")
