86e0abac5ac5d39a42803bf32f02a8977cb3216f
Christophe Gagnier <c.gagnier11@gmail.com>
2025-01-25 15:40:27 -0500

    Player: Can die, New Level: Climb

--- game/components/Player/character_body_2d.gd
+++ game/components/Player/character_body_2d.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 class_name Player
 
+signal died()
 
 @export var SPEED = 140.0
 const JUMP_VELOCITY = -400.0
+
+var will_die = false
+var alive = true
 
 ## State Machine
 enum state {idle, run, jump, falling, landing, hit, die}
@@ -11,7 +15,7 @@ var current_state = null
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-#region States
+#region Statess
 func _enter_state(new_state):
 	
 	if (current_state != new_state):
@@ -28,6 +32,8 @@ func _enter_state(new_state):
 		animated_sprite.play("falling")
 	if new_state == state.landing:
 		animated_sprite.play("landing")
+	if new_state == state.die:
+		_dying()
 
 # The good, the bad and the ugly
 func _try_change_state(new_state):
@@ -57,7 +63,11 @@ func _leave_state(old_state):
 
 func _process_falling():
 	if is_on_floor():
-		_enter_state(state.landing)
+		if will_die:
+			_enter_state(state.die)
+		else:
+			_enter_state(state.landing)
+	
 
 func _process_jumping():
 	if velocity.y >= 0:
@@ -65,42 +75,56 @@ func _process_jumping():
 #endregion
 
 func _ready() -> void:
+	alive = true
+	will_die = false
 	_enter_state(state.idle)
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+		# TODO: Remove dying trigger
+		if velocity.y > 600:
+			will_die = true
 		# Not falling in the context of a jump
 		_try_change_state(state.falling)
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		_enter_state(state.jump)
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
 	
-	if direction:
-		velocity.x = direction * SPEED
-		# Face walking direction
-		animated_sprite.flip_h = direction > 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if velocity.y == 0:
-		if velocity.x != 0.0 :
-			_try_change_state(state.run)
+	if alive:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			_enter_state(state.jump)
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("move_left", "move_right")
+
+		if direction:
+			velocity.x = direction * SPEED
+			# Face walking direction
+			animated_sprite.flip_h = direction > 0
 		else:
-			_try_change_state(state.idle)
-		
-	_process_states()
-	
-	move_and_slide()
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		if velocity.y == 0:
+			if velocity.x != 0.0 :
+				_try_change_state(state.run)
+			else:
+				_try_change_state(state.idle)
+			
+		_process_states()
+		move_and_slide()
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animated_sprite.animation == "landing":
 		_enter_state(state.idle)
+	if animated_sprite.animation == "die":
+		print("died animation completed")
+		died.emit()
+
+func _dying() -> void:
+	alive = false
+	animated_sprite.play("die")
