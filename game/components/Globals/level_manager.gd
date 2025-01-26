d9dc6ed57696671e1a2887d9419ed2a06981e40f
Christophe Gagnier <c.gagnier11@gmail.com>
2025-01-26 12:40:40 -0500

    Camera limits global loaded on level load

--- game/components/Globals/level_manager.gd
+++ game/components/Globals/level_manager.gd
@@ -5,6 +5,10 @@ var next_level_index = 0
 const LEVEL_LIST = preload("res://components/Levels/LevelList2.tres")
 var overlay = preload("res://UI/overlay.tscn")
 
+var level_limit_set = false
+var limits = {"left": 0, "right": 0, "top": 0, "bottom": 0}
+
+
 func go_to_next_level():
 	var _next_level
 	if next_level_index >= LEVEL_LIST.levels.size():
@@ -25,7 +29,22 @@ func go_to_next_level():
 		if current_level:
 			current_level.name = "DiscardedLevel"
 			current_level.queue_free()
-	
+		level_limit_set = false
 		current_level = _next_level_scene
 	else:
 		print("Level data not valid, can't go to next level if it's not valid, please stop being a noob!")
+
+func set_current_camera_limits(left, top, right, bottom) -> void:
+	if not level_limit_set:
+		print("set me up")
+		
+		limits.left = left
+		limits.right = right
+		limits.top = top
+		limits.bottom = bottom
+		level_limit_set = true
+	else:
+		print("already setted limits, try again next time")
+
+func get_current_camera_limits() -> Array:
+	return [limits.left, limits.top, limits.right, limits.bottom]
--- game/components/Player/player_camera.gd
+++ game/components/Player/player_camera.gd
@@ -2,5 +2,14 @@ extends Camera2D
 
 
 func _ready() -> void:
+	if not LevelManager.level_limit_set:
+		LevelManager.set_current_camera_limits(limit_left,limit_top,limit_right,limit_bottom)
+	else:
+		var limits = LevelManager.get_current_camera_limits()
+		limit_left = limits[0]
+		limit_top = limits[1]
+		limit_right = limits[2]
+		limit_bottom = limits[3]
+
 	make_current()
 	get_tree().create_timer(2).timeout.connect(func(): position_smoothing_enabled = true)
