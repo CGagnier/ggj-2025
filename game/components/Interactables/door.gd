a20596fb228f7d9ba61b3819f38b1e6b09a843c1
Julien Esposito <julespo3d@gmail.com>
2025-01-26 11:57:09 -0500

    Fix Door if player already in the collision

--- game/components/Interactables/door.gd
+++ game/components/Interactables/door.gd
@@ -4,6 +4,7 @@ extends AnimatedSprite2D
 @export var can_open = true
 var has_exited = false
 var is_open = false
+var player_in: Player = null
 
 #const PLAYER_SCENE = preload("res://components/Player/player.tscn")
 @export var PLAYER_SCENE: PackedScene
@@ -27,13 +28,17 @@ func anim_finished():
 			spawn()
 		else:
 			is_open = true
-
+			if player_in:
+				go_to_next_level(player_in)
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	# Body should be the player because collision mask
+	player_in = body 
 	if is_open and is_exit and not has_exited:
-		#if body is player
-		has_exited = true
-		body.queue_free()
-		LevelManager.go_to_next_level.call_deferred()
+		go_to_next_level(body)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player_in = null
 
 func spawn() -> void:
 	var player: Player = PLAYER_SCENE.instantiate()
@@ -41,3 +46,8 @@ func spawn() -> void:
 	player.global_position = global_position
 	player.connect("died",open)
 	get_tree().create_timer(1).timeout.connect(close)
+	
+func go_to_next_level(body: Node2D) -> void:
+	has_exited = true
+	body.queue_free()
+	LevelManager.go_to_next_level.call_deferred()
